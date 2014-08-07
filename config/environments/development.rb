98fdcd2fb500d1f55f0c9cf0de72c4db8cd16020
nickrobertson <nick.robertson@octanner.com>
2014-08-07 17:02:22 -0600

    week 4 changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,18 +8,8 @@ Myflix::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :port => 25,
-      :address => "smtp.mailgun.org",
-      :user_name => "postmaster@sandboxf58a098d874b4185b1d3c993b65ce79f.mailgun.org",
-      :password => "890db284410381e1a0bf39d56c9c110d",
-      :domain => "sandboxf58a098d874b4185b1d3c993b65ce79f.mailgun.org",
-      :authentication => :plain }
-
-  #config.action_mailer.delivery_method = :letter_opener
-  #config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
