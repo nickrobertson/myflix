bcdbc7beb35ed0ec750f5bfadfb12978fb8f32da
nickrobertson <nick.robertson@octanner.com>
2014-08-07 14:03:36 -0600

    week 4 changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  include Tokenable
+  #include Tokenable
 
   validates_presence_of :email, :password, :full_name
   validates_uniqueness_of :email
@@ -31,6 +31,11 @@ class User < ActiveRecord::Base
 
   def follow(another_user)
     following_relationships.create(leader: another_user) if can_follow?(another_user)
+  end
+
+  def generate_token
+    self.token = SecureRandom.urlsafe_base64
+    self.save
   end
 
 end
