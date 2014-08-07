ed469e093dd0cf02c06e0bec0b28791dea06e533
nickrobertson <nick.robertson@octanner.com>
2014-08-07 17:06:19 -0600

    week 4 changes

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -18,14 +18,14 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { host: 'sleepy-badlands-5631.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'guarded-brook-5204.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :port           => ENV['MAILGUN_SMTP_PORT'],
       :address        => ENV['MAILGUN_SMTP_SERVER'],
       :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
       :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-      :domain         => 'sleepy-badlands-5631.herokuapp.com',
+      :domain         => 'guarded-brook-5204.herokuapp.com',
       :authentication => :plain
   }
 
